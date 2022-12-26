6af2a6d1d6bfe85f899f61ddd7b372bc93bb9667
Ricardo Rodrigues <ricardobrasfoot@gmail.com>
2022-12-26 20:58:55 -0300

    mudancas css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Localidades from './componentes/Localidades';
+import Logo from './componentes/Logo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Localidades></Localidades>
+        <Logo></Logo>
       </header>
+      <div>    <Localidades></Localidades>
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,11 @@
+import ipam from "../../img/ipam.png"
+import "./estilo.css"
+
+function Logo() {
+    return (
+        <div className="logo">
+            <img src={ipam} alt="ipam"></img>
+        </div>
+    )
+}
+export default Logo
